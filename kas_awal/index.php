459debd6ae8cb7fb6bd46a4675c799a54e7d6916
n00b-sx <64547172+n00b-sx@users.noreply.github.com>
2022-12-26 01:12:01 +0800

    Menambahkan Kas Awal
    Menambahkan tabel yang memuat data Kas Awal

---
+++ kas_awal/index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Kas Awal</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">Buka BOS</a>
+    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavId">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="laporan.php">Laporan</a>
+        </li>
+        <li class="nav-item dropdown active">
+          <a class="nav-link dropdown-toggle" href="#" id="transaksi" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Transaksi</a>
+          <div class="dropdown-menu" aria-labelledby="transaksi">
+            <a class="dropdown-item" href="kas_awal/index.php">Kas Awal</a>
+            <a class="dropdown-item" href="saldo_keluar.php">Saldo Keluar</a>
+            <a class="dropdown-item" href="saldo_masuk.php">Saldo Masuk</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-xs-8 mt-2">
+        <h3>Buku Kas Awal</h3>
+      </div>
+    </div>
+    <div class="container justify-content-center mt-2">
+      <table class="table table-hover">
+        <thead class="thead-default">
+          <tr>
+            <th>No</th>
+            <th>Saldo Bank</th>
+            <th>Saldo Tunai</th>
+            <th>Saldo Pajak Pph21</th>
+            <th>Saldo Pajak Pph22</th>
+            <th>Saldo Pajak Pb1</th>
+            <th>Saldo Pajak PPn</th>
+            <th>Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td scope="row">1</td>
+            <td>3987</td>
+            <td>208187</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>
+              <a href="ubah.php" class="badge badge-info mr-1">Ubah</a>
+              <!-- Button trigger modal -->
+              <a href="#" class="badge badge-danger" data-toggle="modal" data-target="#modelId">Hapus</a>
+
+              <!-- Modal -->
+              <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title">Peringatan!</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                      <div class="container-fluid">
+                        Apakah anda yakin ingin menghapus data ini ?
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                      <button type="button" class="btn btn-primary">Ya</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <script>
+                $('#exampleModal').on('show.bs.modal', event => {
+                  var button = $(event.relatedTarget);
+                  var modal = $(this);
+                  // Use above variables to manipulate the DOM
+
+                });
+              </script>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
