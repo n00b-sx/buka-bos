4e3c8388775671e2ff390125ddf3fe29bf06f77d
n00b-sx <64547172+n00b-sx@users.noreply.github.com>
2023-02-14 14:26:36 +0800

    masih rusak

--- index.php
+++ index.php
@@ -1,112 +1,71 @@
-<?php
-require 'functions.php';
-?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Buka BOS | Log In</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Toastr -->
-  <link rel="stylesheet" href="plugins/toastr/toastr.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BUKA BOS</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="asset/style.css">
 </head>
 
-<body class="hold-transition login-page">
-  <div class="login-box">
-    <!-- /.login-logo -->
-    <div class="card card-outline card-primary">
-      <div class="card-header text-center">
-        <a href="../../index2.html" class="h1">BUKA <b>BOS</b></a>
+<body>
+
+  <!-- Disini Navbar -->
+  <header class="tengah">
+    <div class="nav mt-3">
+      <div class="row m-2">
+        <h1>BUKA BOS</h1>
       </div>
-      <div class="card-body">
-        <p class="login-box-msg">Masuk untuk memulai harimu yang indah</p>
-
-        <form action="" method="POST">
-          <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Username" name="username">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-user"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password" name="password">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <!-- <div class="col-8">
-              <div class="icheck-primary">
-                <input type="checkbox" id="remember">
-                <label for="remember">
-                  Masuk Otomatis
-                </label>
-              </div>
-            </div> -->
-            <!-- /.col -->
-            <div class="col-4">
-              <button type="submit" class="btn btn-primary btn-block" name="login">Sign In</button>
-            </div>
-            <!-- /.col -->
-          </div>
-        </form>
-
-        <!-- <div class="social-auth-links text-center mt-2 mb-3">
-          <a href="#" class="btn btn-block btn-primary">
-            <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-          </a>
-          <a href="#" class="btn btn-block btn-danger">
-            <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-          </a>
-        </div> -->
-        <!-- /.social-auth-links -->
-
-        <p class="mb-1">
-          <a href="forgot-password.html">So dapa lupa password?</a>
-        </p>
-        <p class="mb-0">
-          <a href="register.html" class="text-center">Daftar baru</a>
-        </p>
-      </div>
-      <!-- /.card-body -->
     </div>
-    <!-- /.card -->
-  </div>
-  <!-- /.login-box -->
+  </header>
 
-  <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
-  <!-- Toastr -->
-  <script src="plugins/toastr/toastr.min.js"></script>
+  <!-- Disini Content -->
+  <main class="tengah mt-4">
+    <div class="navtengah">
+      <div class="row">
+        <div class="col">
+          <div class="card text-bg-dark">
+            <img src="asset/img/data.jpg" class="card-img" alt="...">
+            <div class="card-img-overlay">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card text-bg-dark">
+            <img src="asset/img/transaksi.jpg" class="card-img" alt="...">
+            <div class="card-img-overlay">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card text-bg-dark">
+            <img src="asset/img/laporan.jpg" class="card-img" alt="...">
+            <div class="card-img-overlay">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <!-- Disini Footer -->
+  <footer class="tengah">
+    <div class="container">
+
+    </div>
+  </footer>
+
 </body>
-
-<?php
-// Pengecekan Autentikasi Login
-if (isset($_POST['login'])) {
-  if (autentikasi($_POST) > 0) {
-    header('location:app/');
-  } else {
-    echo "<script>toastr.error('Password atau Username yang anda masukan salah');</script>";
-  }
-}
-?>
 
 </html>
