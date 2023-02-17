40b388fda5f3e1b3cb8dcb58fd14b671df5c3deb
n00b-sx <64547172+n00b-sx@users.noreply.github.com>
2023-02-17 12:54:13 +0800

    Tambah Jenis Transaksi, Komponen, Penyedia dan Program

--- master/jenis_transaksi.php
+++ master/jenis_transaksi.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>BUKA BOS | Master</title>
+  <title>BUKA BOS | Master-Jenis Transaksi</title>
   <link rel="stylesheet" href="../asset/css/bootstrap.min.css">
   <link rel="stylesheet" href="../asset/css/style.css">
 </head>
@@ -33,43 +33,19 @@
           <!-- <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
             The current link item
           </a> -->
-          <a href="#" class="list-group-item list-group-item-action active" aria-current="true">Home</a>
+          <a href="master.php" class="list-group-item list-group-item-action">Home</a>
           <a href="program.php" class="list-group-item list-group-item-action">Program</a>
-          <a href="jenis_transaksi.php" class="list-group-item list-group-item-action">Jenis Transaksi</a>
+          <a href="#" class="list-group-item list-group-item-action active" aria-current="true">Jenis Transaksi</a>
           <a href="komponen.php" class="list-group-item list-group-item-action">Komponen</a>
           <a href="penyedia.php" class="list-group-item list-group-item-action">Penyedia</a>
-          <!-- <a class="list-group-item list-group-item-action disabled">A disabled link item</a> -->
         </div>
       </div>
       <div class="col-md-8" style="height : 500px">
         <div class="row">
-          <div class="col-md-6">
-            <div class="card">
-              <div class="card-header bg-danger text-bg-dark">
-                <h5>Program</h5>
-              </div>
-              <div class="card-body">
-                <table class="table table-hover">
-                  <thead>
-                    <tr>
-                      <th>No.</th>
-                      <th>Nama Program</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Program Satu</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
+          <div class="col-md-8">
             <div class="card">
               <div class="card-header bg-info text-bg-dark">
-                <h5>Jenis Transaksi</h5>
+                <h5>Data Jenis Transaksi</h5>
               </div>
               <div class="card-body">
                 <table class="table table-hover">
@@ -79,6 +55,7 @@
                       <th>Kode Transaksi</th>
                       <th>Kode Rekening</th>
                       <th>jenis Transaksi</th>
+                      <th>Aksi</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -90,6 +67,12 @@
                         <td>Kode Transaksi <?= $i ?></td>
                         <td>Kode Rekening <?= $i ?></td>
                         <td>Jenis Transaksi <?= $i ?></td>
+                        <td>
+                          <div class="d-grid gap-1 d-md-block">
+                            <a href="#" class="btn btn-success btn-sm" type="button">Edit</a>
+                            <a href="#" class="btn btn-danger btn-sm" type="button">Hapus</a>
+                          </div>
+                        </td>
                       </tr>
                     <?php endfor; ?>
                   </tbody>
@@ -97,56 +80,36 @@
               </div>
             </div>
           </div>
-        </div>
-        <div class="row mt-2">
-          <div class="col-md-6">
+          <div class="col-md-4">
             <div class="card">
-              <div class="card-header bg-warning text-bg-dark">
-                <h5>Komponen</h5>
+              <div class="card-header bg-info text-bg-dark">
+                <h5>Tambah Data Jenis Transaksi</h5>
               </div>
-              <div class="card-body">
-                <table class="table table-hover">
-                  <thead>
-                    <tr>
-                      <th>No.</th>
-                      <th>Nama Komponen</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Program Satu</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="card">
-              <div class="card-header bg-success text-bg-dark">
-                <h5>Penyedia</h5>
-              </div>
-              <div class="card-body">
-                <table class="table table-hover">
-                  <thead>
-                    <tr>
-                      <th>No.</th>
-                      <th>Nama Penyedia</th>
-                      <th>Nomor Telepon</th>
-                      <th>NPWP</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Program Satu</td>
-                      <td>08xxxx</td>
-                      <td>000.000.00</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+              <form action="POST">
+                <div class="card-body">
+                  <div class="mb-3">
+                    <label for="kodeRekening" class="form-label">Kode Transaksi</label>
+                    <input type="text" class="form-control" id="kodeTransaksi" aria-describedby="kodeTransaksi">
+                    <div id="kodeTransaksi" class="form-text">Masukan Kode Transaksi.</div>
+                  </div>
+                  <div class="mb-3">
+                    <label for="kodeRekening" class="form-label">Kode Rekening</label>
+                    <input type="text" class="form-control" id="kodeRekening" aria-describedby="kodeTransaksi">
+                    <div id="kodeRekening" class="form-text">Masukan Kode Rekening.</div>
+                  </div>
+                  <div class="mb-3">
+                    <label for="jenisTransaksi" class="form-label">Jenis Transaksi</label>
+                    <input type="text" class="form-control" id="jenisTransaksi" aria-describedby="jenisTransaksi">
+                    <div id="jenisTransaksi" class="form-text">Masukan Jenis Transaksi.</div>
+                  </div>
+                </div>
+                <div class="card-footer">
+                  <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button class="btn btn-secondary me-md-2" type="reset">Batal</button>
+                    <button class="btn btn-primary" type="button">Simpan</button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
